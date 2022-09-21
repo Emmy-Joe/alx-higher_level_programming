9d83059977a0b74329071dfccc1888f5b5542033
Emmy-Joe <ochigboje@gmail.com>
2022-09-20 21:18:30 -0700

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
-    if chr(letter) not 101 and chr(letter) not 113:
+    if chr(letter) not 'e' and chr(letter) not 'q':
         print("{}".format(chr(letter)), end="")
