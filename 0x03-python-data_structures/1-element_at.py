85c49ae3b2c84ed503c018452022391bfed8056e
Emmy-Joe <ochigboje@gmail.com>
2022-09-26 17:14:01 -0700

    Task-1:  a function that retrieves an element from a list like in C.

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    lenght = len(my_list, idx) - 1
+    if (idx < 0 or idx > lenght):
+        return (none)
+    else:
+        return (my_list[idx])
